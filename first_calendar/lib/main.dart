4fb1f6b12b2ca79fc0f9ee81ca20e839300a2e8a
Jorge Borges <jorgetrabalho2016@gmail.com>
2023-09-15 09:19:26 -0300

    Create bottom_navigation

--- first_calendar/lib/main.dart
+++ first_calendar/lib/main.dart
@@ -1,47 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:ionicons/ionicons.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(''),
+    Text(''),
+    Text(''),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CalendarApp(),
-    );
-  }
-}
-
-class CalendarApp extends StatefulWidget {
-  @override
-  _CalendarAppState createState() => _CalendarAppState();
-}
-
-class _CalendarAppState extends State<CalendarApp> {
-  DateTime _focusedDay = DateTime.now();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Calendário Flutter'),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            TableCalendar(
-              focusedDay: _focusedDay,
-              firstDay: DateTime.utc(2023, 1, 1),
-              lastDay: DateTime.utc(2023, 12, 31),
-              onPageChanged: (newFocusedDay) {
-                setState(() {
-                  _focusedDay = newFocusedDay;
-                });
-              }
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(''),
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Ionicons.calculator_outline),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Ionicons.chatbubble_outline),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Ionicons.calendar_clear_outline),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Ionicons.reader_outline),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Ionicons.person_circle_outline),
+              label: '',
             ),
           ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black,
+          onTap: _onItemTapped,
         ),
       ),
     );
